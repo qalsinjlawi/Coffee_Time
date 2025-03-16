176bfe40b72b678dceef52aba01ccd0f7d73acaa
Qutaiba Al-Sinjlawi <qalsinjlawi@gmail.com>
2025-03-16 12:43:04 +0300

    Delete template-parts directory

--- template-parts/pagination.php
+++
@@ -1,8 +0,0 @@
-<?php if(get_theme_mod('cafe_elementor_show_pagination', true )== true): ?>
-	<?php
-		the_posts_pagination( array(
-			'prev_text' => esc_html__( 'Previous page', 'cafe-elementor' ),
-			'next_text' => esc_html__( 'Next page', 'cafe-elementor' ),
-		) );
-	?>		
-<?php endif; ?>
